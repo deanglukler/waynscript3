e7d49e3ab5034b432b17e3e843678815b8da9355
Dean Glueckler <glueckler@gmail.com>
2022-06-06 11:03:38 -0700

    edit eslint rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,13 @@ module.exports = {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'error',
+    'import/prefer-default-export': 'off',
     // Since React 17 and typescript 4.1 you can safely disable the rule
     'react/react-in-jsx-scope': 'off',
+    'react/button-has-type': 'off',
+    'no-lone-blocks': 'off',
+    'prefer-destructuring': 'warn',
+    'class-methods-use-this': 'warn',
   },
   parserOptions: {
     ecmaVersion: 2020,
