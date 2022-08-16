56f824d407af10edef79cd879c4f7d796a257fe5
Dean Glueckler <glueckler@gmail.com>
2022-08-16 13:45:10 -0600

    added error tracking and error boundary

--- src/main/main.ts
+++ src/main/main.ts
@@ -19,6 +19,25 @@ import FileScan from './utils/FileScan';
 import Windows from './utils/Windows';
 import { WordsAnalysis } from './utils/WordsAnalysis';
 
+const Bugsnag = require('@bugsnag/electron');
+
+Bugsnag.start({
+  apiKey: '6029e7f07c1a2360d571557de7338d5f',
+  // other init options here..
+  // https://docs.bugsnag.com/platforms/electron/configuration-options/
+});
+
+process.on('unhandledRejection', (reason: any, promise: Promise<any>) => {
+  console.log('\nUnhandled Rejection at:');
+  console.log(reason?.stack || reason);
+  Bugsnag.notify(new Error(reason?.stack || reason));
+});
+process.on('uncaughtException', (err) => {
+  console.log('\nuncaught exception:');
+  console.log(err);
+  Bugsnag.notify(err);
+});
+
 const electronIpcLog = require('electron-ipc-log');
 
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
@@ -93,12 +112,3 @@ app
     });
   })
   .catch(console.log);
-
-process.on('unhandledRejection', (reason: any, promise: Promise<any>) => {
-  console.log('\nUnhandled Rejection at:');
-  console.log(reason?.stack || reason);
-});
-process.on('uncaughtException', (err) => {
-  console.log('\nuncaught exception:');
-  console.log(err);
-});
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,17 +1,41 @@
+// import Bugsnag from '@bugsnag/electron';
+import Bugsnag from '@bugsnag/js';
+import BugsnagPluginReact from '@bugsnag/plugin-react';
+import React from 'react';
+
 import { createRoot } from 'react-dom/client';
+
 import ListApp from './ListApp';
 import QueryApp from './QueryApp';
 
-const listContainer = document.getElementById('list-root')!;
-if (listContainer) {
+import './App.scss';
+import { AppError } from './AppError';
+
+Bugsnag.start({
+  apiKey: '6029e7f07c1a2360d571557de7338d5f',
+  plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
+
+const listContainer = document.getElementById('list-root');
+if (listContainer != null) {
   const root = createRoot(listContainer);
-  root.render(<ListApp />);
+  root.render(
+    <ErrorBoundary FallbackComponent={AppError}>
+      <ListApp />
+    </ErrorBoundary>
+  );
 }
 
-const queryContainer = document.getElementById('query-root')!;
-if (queryContainer) {
+const queryContainer = document.getElementById('query-root');
+if (queryContainer != null) {
   const root = createRoot(queryContainer);
-  root.render(<QueryApp />);
+  root.render(
+    <ErrorBoundary FallbackComponent={AppError}>
+      <QueryApp />
+    </ErrorBoundary>
+  );
 }
 
 // calling IPC exposed from preload script
